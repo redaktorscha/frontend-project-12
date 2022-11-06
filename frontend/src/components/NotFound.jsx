65ec0d28d7ebbd815d9f15fb9853dd8950bf1339
Masha <nasonkina@yandex.ru>
2022-11-07 00:45:19 +0300

    add NotFound component

---
+++ frontend/src/components/NotFound.jsx
@@ -0,0 +1,31 @@
+import {
+  Container, Image, Row, Col,
+} from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import notfound from '../assets/404.svg';
+import Wrapper from './Wrapper';
+
+const NotFound = () => (
+  <Wrapper>
+    <Container fluid>
+      <Row className="min-vh-100 d-flex justify-content-center">
+        <Col className="col-sm-8 col-md-6 col-lg-4 d-flex flex-column align-items-center">
+          <Image
+            fluid
+            src={notfound}
+          />
+          <h1 className="h3 text-muted">Page not found</h1>
+          <small className="text-muted">
+            Would you like to return
+          </small>
+          <Link to="/">to the main page?</Link>
+
+        </Col>
+
+      </Row>
+
+    </Container>
+  </Wrapper>
+);
+
+export default NotFound;
