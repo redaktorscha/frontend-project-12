221390bd0ed4934e018ab4647085a552498e9eca
Masha <nasonkina@yandex.ru>
2022-11-07 01:46:06 +0300

    add App (routes)

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -4,15 +4,20 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Chat from './Chat';
 import Login from './Login';
 import Signup from './Signup';
+import NotFound from './NotFound';
+import AuthProvider from './AuthProvider';
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Chat />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Signup />} />
-    </Routes>
-  </BrowserRouter>
+  <AuthProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Chat />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  </AuthProvider>
 );
 
 export default App;
