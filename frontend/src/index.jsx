ca0250e8cc7b5cd4a341dafbfddbd58d8005435f
Masha <nasonkina@yandex.ru>
2023-01-05 13:50:42 +0300

    add Rollbar

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,6 +1,7 @@
 // ts-check
 import React from 'react';
-import { Provider } from 'react-redux';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
+import { Provider as ReduxProvider } from 'react-redux';
 import { I18nextProvider } from 'react-i18next';
 import { createRoot } from 'react-dom/client';
 import App from './components/App';
@@ -15,17 +16,28 @@ import AuthProvider from './components/AuthProvider';
 
   const { i18n, socketFunctions } = await initApp();
 
+  const rollbarConfig = {
+    accessToken: process.env.ROLLBAR_APP_ACCESS_TOKEN,
+    environment: 'production',
+  };
+
   root.render(
     <React.StrictMode>
-      <Provider store={store}>
-        <I18nextProvider i18n={i18n}>
-          <AuthProvider>
-            <SocketProvider functions={socketFunctions}>
-              <App />
-            </SocketProvider>
-          </AuthProvider>
-        </I18nextProvider>
-      </Provider>
+      <RollbarProvider config={rollbarConfig}>
+        <ErrorBoundary>
+          <ReduxProvider store={store}>
+            <I18nextProvider i18n={i18n}>
+              <AuthProvider>
+                <SocketProvider functions={socketFunctions}>
+                  <App />
+                </SocketProvider>
+              </AuthProvider>
+            </I18nextProvider>
+          </ReduxProvider>
+        </ErrorBoundary>
+
+      </RollbarProvider>
+
     </React.StrictMode>,
   );
 })();
