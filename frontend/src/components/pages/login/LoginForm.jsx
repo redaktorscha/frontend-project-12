d7ad0906161d1d162748bb90b829dba321d1ca2e
Masha <nasonkina@yandex.ru>
2023-01-31 17:36:24 +0300

    fix LoginForm

--- frontend/src/components/pages/login/LoginForm.jsx
+++ frontend/src/components/pages/login/LoginForm.jsx
@@ -1,11 +1,23 @@
 // ts-check
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import * as yup from 'yup';
 import { useTranslation } from 'react-i18next';
-import UserForm from '../../UserForm';
+import { toast } from 'react-toastify';
+import { Form, Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import { Formik } from 'formik';
+import axios from 'axios';
+import { useRollbar } from '@rollbar/react';
+import { appRoutes, LOGIN_ENDPOINT } from '../../../utils/routes';
+import { useAuth } from '../../../hooks';
 
 const LoginForm = () => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+  const { setUser } = useAuth();
+  const rollbar = useRollbar();
+
+  const [formLoginError, setFormLoginError] = useState('');
 
   const loginSchema = yup
     .object()
@@ -20,34 +32,122 @@ const LoginForm = () => {
         .required(t('errors.login.required')),
     });
 
-  const inputs = [
-    {
-      type: 'text',
-      inputName: 'username',
-      ref: useRef(null),
-      label: t('ui.login.username'),
-    },
-    {
-      type: 'password',
-      inputName: 'password',
-      ref: useRef(null),
-      label: t('ui.login.password'),
-    },
-  ];
+  const initialValues = {
+    username: '',
+    password: '',
+  };
 
-  const buttonText = t('ui.login.btnLogin');
+  const inputUsername = useRef(null);
+  const inputPassword = useRef(null);
 
-  const renderData = {
-    inputs, buttonText,
+  useEffect(() => {
+    if (formLoginError !== '') {
+      [inputUsername, inputPassword].forEach((ref) => {
+        ref.current.classList.remove('is-valid');
+        ref.current.classList.add('is-invalid');
+      });
+    }
+  }, [formLoginError]);
+
+  const handleSubmitLogin = async (formData) => {
+    const route = appRoutes[LOGIN_ENDPOINT]();
+    try {
+      setFormLoginError('');
+      const response = await axios.post(route, formData);
+      const { data } = response;
+      if (data) {
+        setUser(data);
+        navigate('/');
+      }
+    } catch (e) {
+      if (e.response.status === 401) {
+        setFormLoginError(t('errors.login.invalid'));
+      } else {
+        rollbar.error('login error', e);
+        toast.error(t('toasts.networkError'));
+      }
+      setUser(null); // ? remove?
+    }
   };
 
   return (
-    <UserForm
-      eventType="login"
-      errorCode={401}
-      renderData={renderData}
+    <Formik
       validationSchema={loginSchema}
-    />
+      initialValues={initialValues}
+      validateOnChange={false}
+      validateOnBlur={false}
+      onSubmit={async (values) => {
+        await handleSubmitLogin(values);
+      }}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        values,
+        touched,
+        errors,
+        isSubmitting,
+      }) => (
+        <Form
+          noValidate
+          onSubmit={handleSubmit}
+        >
+
+          <Form.Group
+            key="username"
+            className="form-floating mb-4"
+            controlId="f-username"
+          >
+            <Form.Control
+              type="text"
+              name="username"
+              autoComplete="off"
+              required
+              placeholder={t('ui.login.username')}
+              value={values.username}
+              onChange={handleChange}
+              isValid={touched.username && !errors.username}
+              isInvalid={touched.username && !!errors.username}
+              ref={inputUsername}
+            />
+            <Form.Label>{t('ui.login.username')}</Form.Label>
+            <Form.Control.Feedback type="invalid" tooltip>
+              {errors.username}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group
+            key="password"
+            className="form-floating mb-4"
+            controlId="f-password"
+          >
+            <Form.Control
+              type="password"
+              name="password"
+              autoComplete="off"
+              required
+              placeholder={t('ui.login.password')}
+              value={values.password}
+              onChange={handleChange}
+              isValid={touched.password && !errors.password}
+              isInvalid={touched.password && !!errors.password}
+              ref={inputPassword}
+            />
+            <Form.Label>{t('ui.login.password')}</Form.Label>
+            <Form.Control.Feedback type="invalid" tooltip>
+              {errors.password || formLoginError}
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Button
+            type="submit"
+            variant="outline-primary"
+            className="w-100 mb-3"
+            disabled={isSubmitting}
+          >
+            {t('ui.login.btnLogin')}
+          </Button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
